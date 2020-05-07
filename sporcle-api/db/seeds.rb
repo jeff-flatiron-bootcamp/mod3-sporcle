d3445344f24bf3ab16f7b8e3553d4430d581e4d7
github username <github email address>
2020-05-07 10:58:03 -0700

    Added seeds - could use a few more

--- sporcle-api/db/seeds.rb
+++ sporcle-api/db/seeds.rb
@@ -24,14 +24,37 @@ I shake it off, I shake it off"
 swift_lyric_2 = "Cause baby now we got bad blood
 You know it used to be mad love
 So take a look at what you've done
-'Cause baby now we got bad blood"
+Cause baby now we got bad blood"
+
+eagles_lyric_1 = "Welcome to the Hotel California
+Such a lovely place (such a lovely place)
+Such a lovely face
+Plenty of room at the Hotel California
+Any time of year (any time of year)
+You can find it here"
+
+feist_lyric = "Oh, uh, oh, you're changing your heart
+Oh, uh, oh, you know who you are"
+
+winehouse_lyric = "He walks away
+The sun goes down
+He takes the day, but I'm grown
+And in your way
+In this blue shade
+My tears dry on their own"
 
 
 artistA = Artist.create(name: "Rick Astley");
 artistB = Artist.create(name: "Taylor Swift");
+artistC = Artist.create(name: "The Eagles")
+artistD = Artist.create(name: "Feist")
+artistE = Artist.create(name: "Amy Winehouse")
 
 song1ArtistA = Song.create(title: "Never Gonna Give You Up", lyric: astley_lyric, artist_id: artistA.id)
-song1ArtistA = Song.create(title: "Shake It Off", lyric: swift_lyric_1, artist_id: artistB.id)
-song1ArtistA = Song.create(title: "Never Gonna Give You Up", lyric: swift_lyric_2, artist_id: artistB.id)
+song1ArtistB = Song.create(title: "Shake It Off", lyric: swift_lyric_1, artist_id: artistB.id)
+song2ArtistB = Song.create(title: "Never Gonna Give You Up", lyric: swift_lyric_2, artist_id: artistB.id)
+song1ArtistC = Song.create(title: "Hotel California", lyric: eagles_lyric_1, artist_id: artistC.id)
+song1ArtistD = Song.create(title: "1234", lyric: feist_lyric, artist_id: artistD.id)
+song1ArtistE = Song.create(title: "Tears Dry On Their Own", lyric: winehouse_lyric, artist_id: artistE.id)
 
 puts "Seeded file";
