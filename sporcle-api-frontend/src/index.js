710939ab6880892aac65783147e8739a819b4384
github username <github email address>
2020-05-07 17:25:10 -0700

    final css

--- sporcle-api-frontend/src/index.js
+++ sporcle-api-frontend/src/index.js
@@ -140,6 +140,14 @@
 
     }
 
+        //build title
+        titleDiv=document.createElement('div')
+        titleDiv.id="titleDiv"
+        appTitle=document.createElement('p')
+        appTitle.innerText="Sporcle Distancing"
+        titleDiv.appendChild(appTitle)
+        btnDiv.appendChild(titleDiv)
+
     //build input box
     function buildInputBox(){
         inputDiv.innerHTML = `
