994e2f7e83cd50e170ef8c25cfda6968b197375f
dizhengcindy <dizhengcindy@gmail.com>
2020-05-08 09:11:35 -0700

    added song title and artist name

--- sporcle-api-frontend/src/index.js
+++ sporcle-api-frontend/src/index.js
@@ -20,6 +20,7 @@
     //score
     let scoreDiv = document.createElement("div")
     scoreDiv.id="scoreDiv"
+    scoreDiv.style.display = "none"
     let score = document.createElement("span")
     let lyricsCountNumber = 0
     //countdown timer
@@ -34,7 +35,9 @@
     giveUpBtn.id = "giveUp"
     //lyric box
     let lyricsDiv = document.createElement("table")
-
+    //songTitle
+    let songTitleDiv = document.createElement("div")
+    let songTitle = document.createElement("p")
     let artistId
     let currentHint
     //*************build HTML elements*************************************************************************************
@@ -75,7 +78,7 @@
         randArtistName = artists[randArtist].name
         // artistSelect.addEventListener("change", selectArtistHandler)
         artistBtnDiv.appendChild(artistSelect)
-        btnDiv.appendChild(artistBtnDiv)
+       // btnDiv.appendChild(artistBtnDiv)
     }
 
     //build start game button
@@ -95,7 +98,9 @@
             hintDiv.style.display = "block"        
             giveUpDiv.style.display = "block"
             artistSelect.style.display = "none"
-            artistBtnDiv.innerHTML = "" 
+            scoreDiv.style.display = "block"
+            artistBtnDiv.innerHTML = "Artist: "+ artistName
+            songTitle.style.display = "block"
             //start timer
             let countdown = document.getElementById("timer")
             startTimer(countDownMinutes, countdown)
@@ -158,7 +163,7 @@
         function showHintWord(data){
             currentHint.classList.toggle("current_hint")
             currentHint.innerText = data.hint
-            scoreDiv.innerText = data.total + "/" + scoreDiv.innerText.split("/")[1]
+            scoreDiv.innerText = "Score: "+data.total + "/" + scoreDiv.innerText.split("/")[1]
         }
         hintContentDiv.style.display = "none"
 
@@ -213,7 +218,7 @@
             lyricsDiv.appendChild(td)
         }
         mainElm.appendChild(lyricsDiv)
-        scoreDiv.innerText = `0/${lyricsCount}`
+        scoreDiv.innerText = "Score: " +`0/${lyricsCount}`
     }
 
     //custom message based on timeout
@@ -253,7 +258,25 @@
         guessBox.style.borderColor="white"
         guessBox.innerHTML=guess[0]
     }
+    //give out song name
+    function buildSongTitleHint(){
 
+        songTitle.innerText = "Song Title?"
+        songTitle.style.display = "none"
+        songTitle.addEventListener("click", function(){
+            fetchSongTitle(showSongTitle)
+
+        })
+        songTitleDiv.appendChild(songTitle)
+        btnDiv.appendChild(artistBtnDiv)
+        btnDiv.appendChild(songTitleDiv)
+    }
+
+    function showSongTitle(data){
+        songTitle.innerText = "Song: " + data.song_title
+        scoreDiv.innerText = "Score: "+data.total + "/" + scoreDiv.innerText.split("/")[1]
+
+    }
     //*************put all HTML elements together *************************************************************************************
     //call all the build
     function btnForm(){
@@ -261,6 +284,7 @@
     mainElm.appendChild(btnDiv)
     //select artists
     fetchAllArtists(buildArtistsSelection)
+    buildSongTitleHint()
     //start
     buildStartGameBtn()
     //input box
